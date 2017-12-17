6b8bf4e15e7796b7e16b03c2bcf48ab66d696c43
larperdoodle <larsbenedetto@gmail.com>
2017-12-17 02:21:04 -0800

    Made good progress on import form

--- recalls/js/CoreLogic.js
+++ recalls/js/CoreLogic.js
@@ -1,26 +1,28 @@
+var proxy = 'https://cors-anywhere.herokuapp.com/';
+//https://api.nhtsa.gov/vehicles/byVin?productDetail=all&data=none&vin=2GCEK133681303585&name=
 $("#vin_input").on('keyup', function (e) {
     if (e.keyCode == 13) {
         var data = $('#vin_input').val();
-        $.get("https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/" + data + "?format=json",
-            handleDecodedVin)
+	    $.get("https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/" + data + "?format=json",
+		    handleDecodedVin)
+        $.get("https://api.nhtsa.gov/vehicles/byVin?productDetail=all&data=none&vin=" + data + "&name=",
+	        handleRecallInfoNTSHA)
     }
 });
+
 function handleDecodedVin(response){
-    var make, model, year;
-    var res = response["Results"];
-    for(var i =0; i < res.length; i++){
-        var resi = res[i];
-        var name = resi["Variable"];
-        switch(name){
-            case "Make"://5
-                make = resi["Value"];
-                break;
-            case "Model"://7
-                model = resi["Value"];
-                break;
-            case "Model Year"://8
-                year = resi["Value"]
-        }
-    }
-    console.log(data);
+	var res = response["Results"];
+	var make = res[5]["Value"];
+
 }
+
+function handleRecallInfoNTSHA(response){
+    console.log(response);
+}
+
+//Ford
+//https://owner.ford.com/sharedServices/decodevin.do?vin=1FTSW21R68EB99716
+//https://owner.ford.com/sharedServices/recalls/query.do?country=USA&langscript=LATN&language=EN&region=US&vin=1FTSW21R68EB99716
+
+//GM
+//https://my.gm.com/services/2G1WF52E129159920/recalls
